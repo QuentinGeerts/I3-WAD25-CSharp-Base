b231383b63ca75c011bd5d1fa2871e5a9e5e330c
Quentin Geerts <geerts.quentin@gmail.com>
2025-11-25 16:06:07 +0100

    Démonstration 04 : If, switch

---
+++ Demo04/Program.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * Démonstration 04 - Structures conditionnelles
+ */
+
+
+// 1.  if ... else if ... else ...
+
+// 1.1.  if ...
+
+if (true)
+{
+    Console.WriteLine("Exécution du bloc d'instructions");
+}
+
+if (false)
+{
+    Console.WriteLine($"Non exécution du bloc d'instructions");
+}
+
+// 1.2.  if ... else ...
+
+if (true)
+{
+    Console.WriteLine($"Exécuté...");
+}
+else
+{
+    Console.WriteLine("Ne sera pas exécuté...");
+}
+
+// --
+
+if (false)
+{
+    Console.WriteLine("Ne sera pas exécuté...");
+}
+else
+{
+    Console.WriteLine($"Exécuté...");
+}
+
+// 1.3.  Raccourci (sans accolade)
+
+if (true) Console.WriteLine($"Code exécuté..");
+
+if (true) 
+    Console.WriteLine($"Code exécuté..");
+
+// 1.4.  if ... else if ... else ...
+
+if (false)
+{
+    Console.WriteLine($"Non exécuté");
+}
+else if (true)
+{
+    Console.WriteLine($"Exécuté");
+}
+else if (true)
+{
+    Console.WriteLine($"Exécuté");
+}
+else
+{
+    Console.WriteLine($"Non exécuté");
+}
+
+
+// 2.  switch ...
+
+var jour = "mardi";
+
+switch (jour)
+{
+    case "lundi":
+        Console.WriteLine($"Badminton");
+        break;
+
+    case "mardi":
+    case "jeudi":
+        Console.WriteLine($"Jeux de société");
+        break;
+
+    case "mercredi":
+        Console.WriteLine($"Lecture");
+        break;
+
+    default:
+        Console.WriteLine($"Entrée incorrecte");
+        break;
+}
--- readme.md
+++ readme.md
@@ -7,8 +7,9 @@
 - Demo01 : Console, Variables
 - Demo02 : Data-types (C# & CTS), valeur vs référence, concaténation
 - Demo03 : Conversion, cast implicite/explicite, boxing/unboxing
+- Demo04 : If, switch
 
 ---
 
 ## Exercices
-- 
+- Exo : 2 nombres entrés par l'utilisateur avec Parse et TryParse
