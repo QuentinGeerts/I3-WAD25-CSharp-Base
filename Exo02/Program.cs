e3f2bd02ce46c0069a3565cf0d9c934f3031d6ef
Samuel Legrain <samuel.legrain@bstorm.be>
2025-11-27 10:53:33 +0100

    Ajout Exo02

---
+++ Exo02/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Veuillez introduire un nombre");
+string input = Console.ReadLine()!;
+int nombre = int.Parse(input);
+int div = nombre / 2;
+
+if(div + div == nombre)
+{
+    Console.WriteLine("Le nombre est pair");
+}
+else
+{
+    Console.WriteLine("Le nombre est impair");
+}
