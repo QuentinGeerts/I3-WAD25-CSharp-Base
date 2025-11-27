2e78621616ce78260ce131fdd937e9b873119620
Samuel Legrain <samuel.legrain@bstorm.be>
2025-11-27 10:53:55 +0100

    Mise à jour Exo02 (contrôle d'entrée)

--- Exo02/Program.cs
+++ Exo02/Program.cs
@@ -1,7 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Veuillez introduire un nombre");
 string input = Console.ReadLine()!;
-int nombre = int.Parse(input);
+int nombre;
+bool isConverted = int.TryParse(input, out nombre);
+if (isConverted)
+{
+    int div = nombre / 2;
+
+    if (div + div == nombre)
+    {
+        Console.WriteLine($"Le nombre {nombre} est pair");
+    }
+    else
+    {
+        Console.WriteLine($"Le nombre {nombre} est impair");
+    }
+}
+else {
+    Console.WriteLine($"La valeur \"{input}\" n'est pas un nombre...");
+}
+
+/*int nombre = int.Parse(input);
 int div = nombre / 2;
 
 if(div + div == nombre)
@@ -11,4 +30,4 @@ if(div + div == nombre)
 else
 {
     Console.WriteLine("Le nombre est impair");
-}
+}*/
