d4902382eb1e8ec06ba4cc28ceabe6e928b1c847
Samuel Legrain <samuel.legrain@bstorm.be>
2025-11-27 16:14:48 +0100

    Add Exo04

---
+++ Exo04/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exo04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Veuillez introduire votre BBAN (les 12 chiffres) : ");
+            string bban = Console.ReadLine()!;
+            if(bban.Length != 12 || !long.TryParse(bban,out _))
+            {
+                Console.WriteLine("Le BBAN doit contenir exactement 12 chiffres.");
+            }
+            else
+            {
+                string tenFirst = bban.Substring(0, 10);
+                string twoLast = bban.Substring(10);
+                long tenNumbers = long.Parse(tenFirst);
+                short twoNumbers = short.Parse(twoLast);
+                short modulo = (short)(tenNumbers % 97);
+                if ((modulo == 0 && twoNumbers == 97) || (modulo == twoNumbers))
+                {
+                    Console.WriteLine("OK");
+                }
+                else
+                {
+                    Console.WriteLine("KO");
+                }
+            }
+        }
+    }
+}
