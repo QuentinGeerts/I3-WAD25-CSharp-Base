1a4afedf513814f69d52335c20f4f6ab67bbd02c
Samuel Legrain <samuel.legrain@bstorm.be>
2025-11-28 13:42:41 +0100

    Add Exo06

---
+++ Exo06/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exo06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (double i = 0.0; i <= 20.0; i += 0.1) {
+            //    Console.WriteLine(i);
+            //}
+
+            for(decimal i = 0.0m; i <= 20.0m; i += 0.1m) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
